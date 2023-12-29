44962f79ba75b9571c8a3c364f748799afc882b8
ShinhyeL <112687898+ShinhyeL@users.noreply.github.com>
2023-12-29 22:01:00 +0900

    Update 연산자 끼워넣기.py

--- 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.py
+++ 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.py
@@ -6,13 +6,13 @@ answer = []
 # cal=현재까지 결과값. i=리스트 A의 인덱스
 def backtrack(add, sub, mul, div, cal = 0, i = 0):
     
-    #add, sub, mul, div가 모두 0이라면
+    #add, sub, mul, div가 모두 0이라면, 즉 모든 연산이 끝났다면
     if add==0 and sub==0 and mul==0 and div==0 :
         answer.append(cal) #현재 계산된 값들을 answer에 append
         return
     
     if add>0: #현재까지 결과값인 cal에 A[i]를 더하고 다음 인덱스로 넘어간다.
-        backtrack(add-1, sub, mul, div, cal + A[i], i+1)
+        backtrack(add-1, sub, mul, div, cal + A[i], i+1) #A[0]에서부터 시작해서 add가 0이 될때까지 i 하나씩 늘려주며 재귀
     if sub>0:
         backtrack(add, sub-1, mul, div, cal - A[i], i+1)
     if mul>0:
@@ -23,6 +23,6 @@ def backtrack(add, sub, mul, div, cal = 0, i = 0):
         else:
             backtrack(add, sub, mul, div-1, int(cal/A[i]), i+1)
             
-backtrack(add, sub, mul, div, A[0], 1)
+backtrack(add, sub, mul, div, A[0], 1) #리스트의 첫번째 값에서부터 시작
 print(max(answer))
 print(min(answer))
